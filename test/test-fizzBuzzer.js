17b6f169198ace255828ba7899b0fd82ffa88172
Jordan Heffernan <jheff2@gmail.com>
2018-12-21 08:40:16 -0800

    added normal test cases

--- test/test-fizzBuzzer.js
+++ test/test-fizzBuzzer.js
@@ -2,3 +2,13 @@ const expect = require('chai').expect;
 
 const fizzBuzzer = require('../fizzBuzzer');
 
+describe('fizzBuzzer' function() {
+
+  
+  it('should return fizz-buzz if a number is divisable by 15');
+  it('should return fizz if a number is divisable by 3');
+  it('should return buzz if a number is divisable by 5');
+
+
+
+});
